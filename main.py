77e719bac7be246a3844a019ff47ce27ce4cb813
Vislyy <maksimslepuch@gmail.com>
2025-03-16 16:23:02 +0200

    Update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Імпортування бібліотек
-from PyQt5.QtWidgets import *
+from PyQt6.QtWidgets import *
 from file_manager import *
 
 #Головний словник для заміток
@@ -24,9 +24,9 @@ tags_list_lbl = QLabel("Список тегів")
 save_note_btn = QPushButton('Зберегти замітку')
 create_note = QPushButton('Створити замітку')
 delete_note_btn = QPushButton('Видалити замітку')
-search_tag = QPushButton('Шукати замітки по тегу')
+search_tag_btn = QPushButton('Шукати замітки по тегу')
 add_tag_btn = QPushButton('Додати до замітки')
-delete_tag = QPushButton('Відкріпити від замітки')
+delete_tag_btn = QPushButton('Відкріпити від замітки')
 search_tag_area = QLineEdit('Введіть тег...')
 #Підтягування всіх заміток та тегів
 notes_list.addItems(notes)
@@ -42,8 +42,8 @@ v1.addWidget(tags_list)
 v1.addWidget(search_tag_area)
 v1.addLayout(h2)
 h2.addWidget(add_tag_btn)
-h2.addWidget(delete_tag)
-v1.addWidget(search_tag)
+h2.addWidget(delete_tag_btn)
+v1.addWidget(search_tag_btn)
 
 #Створення функцій
 def show_note():
@@ -81,33 +81,41 @@ def delete_note():
     notes_list.addItems(notes)
     write_in_file(notes)
 def add_tag():
+    current_note_status = notes_list.currentItem()
     tag_name, ok = QInputDialog.getText(window, "Новий тег", "Введіть назву тегу")
-    if tag_name and tag_name not in notes[notes_list.currentItem()]["теги"]:
-        notes[notes_list.currentItem().text()]["теги"].append(tag_name)
+    if ok and current_note_status and tag_name not in current_note_status.text["теги"]:
+        notes[current_note_status().text]["теги"].append(tag_name)
         tags_list.clear()
         tags_list.addItems(notes[notes_list.currentItem().text()]["теги"])
         write_in_file(notes)
     else:
-        QMessageBox.Information(window, "Виберіть замітку та перервірте чи немає вже такого тега на замітці")
+        msg = QMessageBox(window)
+        msg.setWindowTitle("Теги: Помилка")
+        msg.setText("Виберіть замітку та перервірте чи немає вже такого тега на замітці")
+        msg.setIcon(QMessageBox.Icon.Information)
+        msg.exec()
 def delete_tag():
-    key = tags_list.currentItem().text()
+    key = notes_list.currentItem().text()
     current_tag_status = tags_list.currentItem()
+    print(f"[DEBUG] Current tag status: {current_tag_status}")
     if current_tag_status:
-        notes.pop(key["теги"])
+        notes[key]["теги"].remove(tags_list.currentItem().text())
         tags_list.clear()
         tags_list.addItems(notes[key]["теги"])
         write_in_file(notes)
     else:
-        msg = QMessageBox()
-        msg.setIcon(QMessageBox.Information)
-        msg.setWindowTitle("Теги")
-        msg.setText("Ви не обрали теги для видалення. \n Будь ласка оберіть теги які ви хочете видалити" )
+        msg = QMessageBox(window)
+        msg.setWindowTitle("Теги: Помилка")
+        msg.setText("Оберіть тег який хочете видалити")
+        msg.setIcon(QMessageBox.Icon.Information)
+        msg.exec()
 #Прив'язка функцій до кнопок
 notes_list.itemClicked.connect(show_note)
 save_note_btn.clicked.connect(save_note)
 create_note.clicked.connect(add_note)
 delete_note_btn.clicked.connect(delete_note)
 add_tag_btn.clicked.connect(add_tag)
+delete_tag_btn.clicked.connect(delete_tag)
 
 #Додавання основи на головну лінію
 mainline.addWidget(text_area)
